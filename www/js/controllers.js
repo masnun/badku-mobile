8113063d8c518670f4b6a81f4df9e8ebdc566970
Abu Ashraf Masnun <masnun@gmail.com>
2016-11-28 13:49:08 +0600

    Basic version

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -13,10 +13,10 @@ angular.module('badku.controllers', [])
 
     $scope.faculties = [
       {
-        "id": "mizanur_rahman",
-        "name": "Mizanur Rahman",
+        "id": "feroz_ahmed",
+        "name": "Feroz Ahmed",
         "position": "Professor",
-        "description": "Head of the discipline",
+        "description": "Feroz Ahmed is currently serving as a Professor at Business Administration Discipline of Khulna University in Bangladesh. He is also in charge of Head of Human Resource Management Discipline.",
         "photo": "no_photo.png",
         "research": [
           {
@@ -25,7 +25,29 @@ angular.module('badku.controllers', [])
             "description": "This study proposes a comprehensive model that includes the antecedents and sustainable outcomes of ecotourism. The research incorporated a mixed method research design and data analyses were conducted by NVivo 10 and PLS SEM. The results identified that two major types of antecedents (resource dimension and support dimension) are needed for ecotourism, which then produces economic, environmental, socio-cultural and emotional benefits in the destination. The analysis further identified the mediation and moderating effects of the outcomes."
           }
         ]
+      },
+
+      {
+        "id": "mizanur_rahman",
+        "name": "Mizanur Rahman",
+        "position": "Professor",
+        "description": "Md. Mizanur Rahman is currently serving as a Professor and also the Head at Business Administration Discipline of Khulna University in Bangladesh. ",
+        "photo": "no_photo.png",
+
+      },
+
+      {
+        "id": "sm_arifuzzaman",
+        "name": "S. M. Arifuzzaman",
+        "position": "Professor",
+        "description": "Md. Mizanur Rahman is currently serving as a Professor and also the Head at Business Administration Discipline of Khulna University in Bangladesh. ",
+        "photo": "sm_arifuzzaman.png",
+
       }
+
+
+
+
     ];
 
     $scope.faculties_data = {};
